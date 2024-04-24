7fdb8a4841c4adcd8f8d408d71e55f428508b950
vanekroman <roman.vanek20@gmail.com>
2024-04-25 00:21:08 +0200

    release 1.0

--- task1_AT_NB.py
+++ task1_AT_NB.py
@@ -7,7 +7,8 @@ import _thread
 import ahtx0
 import ujson
 
-WAIT_FOR_BG77_RESPONSE_TIMEOUT = 20;
+# wait timeout in seconds for sending JSON data function
+WAIT_FOR_BG77_RESPONSE_TIMEOUT = 5;
 
 i2c1 = I2C(1, scl = machine.Pin(15), sda = machine.Pin(14), freq = 400000)
 tmp_sensor = ahtx0.AHT20(i2c1)
@@ -25,6 +26,7 @@ print(bg_uart.read(10))
 module = BG77.BG77(bg_uart, verbose=True, radio = False)
 time.sleep(3)
 
+# ADC current reading task
 def core2_task():
     sel0 = machine.Pin(2, machine.Pin.OUT)
     sel1 = machine.Pin(3, machine.Pin.OUT)
@@ -48,6 +50,7 @@ def core2_task():
         uart.write(str(time.ticks_ms())+ "," + str(read_adc)+"\r")
         time.sleep(0.001)
 
+# wait fuction for the CONNECTED status
 def waitForCEREG():
     data_out = ""
     while True:
@@ -63,9 +66,11 @@ def waitForCEREG():
                 print(data_out)
                 return
 
+# terminal handling fuction 1
 def read1():
     return(sys.stdin.read(1) if spoll.poll(0) else None)
 
+# terminal handling fuction 2
 def readline():
     c = read1()
     buffer = ""
@@ -89,24 +94,28 @@ def __read(self, exit_condition="OK\r\n", timeout = 10) -> str:
     # or: return None # None return handling need to be implemented further within the BG77.py library
     return ""
 
+# fuction sends json data to MQTT topic
 def sendData_JSON(self, data = {}, topic = "telemetry"):
     msg = ujson.dumps(data)
-    if module.sendCommand(f"AT+QMTPUB=1,0,0,0,\"v1/devices/me/" + str(topic) + "\"," + str(len(msg)) + "\r\n", ">", WAIT_FOR_BG77_RESPONSE_TIMEOUT) is None:
-        raise OSError("BG77 Timeout: Cannot get the response from the device.")
-    if module.sendCommand(msg, "+QMTPUB: 1,0,0\r\n", WAIT_FOR_BG77_RESPONSE_TIMEOUT) is None:
-        raise OSError("BG77 Timeout: Cannot get the response from the device.")
+    if module.sendCommand(f"AT+QMTPUB=1,0,0,0,\"v1/devices/me/" + str(topic) + "\"," + str(len(msg)) + "\r\n", ">", WAIT_FOR_BG77_RESPONSE_TIMEOUT) is "":
+        print("BG77 Timeout: Cannot get the response from the device.")
+    if module.sendCommand(msg, "+QMTPUB: 1,0,0\r\n", WAIT_FOR_BG77_RESPONSE_TIMEOUT) is "":
+        print("BG77 Timeout: Cannot get the response from the device.")
 
+module.setRATType(1, 1)
+time.sleep(5)
 module.setRadio(1)
 time.sleep(1)
 module.setAPN("lpwa.vodafone.iot")
-time.sleep(1)
+waitForCEREG()
 module.setOperator(BG77.COPS_MANUAL, BG77.Operator.CZ_VODAFONE)
 time.sleep(1)
-waitForCEREG()
 
 print(" _______________________ \n")
 print("| Micro Weather Station |\n")
 print("|_____connecting ...____|\n")
+
+# start new thread for adc current measurement
 second_thread = _thread.start_new_thread(core2_task, ())
 
 # Using lambda to bind the function with the module instance
@@ -116,13 +125,12 @@ module.sendData_JSONVARIABLE = lambda variable_name, value, topic: sendData_JSON
 # binding rewritten function for BG77
 module.__read = lambda exit_condition="OK\r\n", timeout = WAIT_FOR_BG77_RESPONSE_TIMEOUT: __read(module, exit_condition, timeout)
 
+# connect to the MQTT server
 module.sendCommand("AT+QCSCON=1\r\n")
 module.sendCommand("AT+QMTCFG=\"version\",1,4\r\n")
 module.sendCommand("AT+QMTOPEN=1,\"147.229.146.40\",1883\r\n", "+QMTOPEN: 1,0", 5)
 module.sendCommand("AT+QMTCONN=1,\"27a26100-fd78-11ee-b0f6-299dee8b9dbf\",\"gCVn0rC7cOM7rxvYfo5l\",\"\"\r\n", "+QMTCONN: 1,0,0", 5)
 
-# module.sendCommand("AT+QCFG=\"nwscanseq\",03,1\r\n")
-# time.sleep(1)
 # send device attributes
 device_info = {
     "manufacturer": "micro-weather s.r.o",
@@ -138,8 +146,9 @@ gps = {
 }
 module.sendData_JSON(gps, "attributes")
 
-TAU_TIME = "01100011"
-ACTIVE_TIME = "00000001"
+# requested PSM delay times
+TAU_TIME = "10100001" # 60 s
+ACTIVE_TIME = "00001000" # 16 s
 
 # setup power saving mode
 module.sendCommand("AT+QPSMS=1,,,\"" + TAU_TIME + "\",\"" + ACTIVE_TIME + "\"\r\n")
@@ -151,11 +160,10 @@ print("ACTIVE TIME= " + str(active_time) + "\r\n")
 
 print(" _______________________ \n")
 print("| Micro Weather Station |\n")
-print("|_____measuring ..._____|\n")
-print("Terminal Ready\r\n")
+print("|_______________________|\n")
 
-# APP RDY
 while True:
+    # terminal handling
     data = readline()
     if len(data) != 0:
         if "WKUP" in data.upper():
@@ -171,6 +179,7 @@ while True:
             bg_uart.write(data[:len(data)-2].encode())
             bg_uart.write("\r\n")
 
+    # BG77 response handling
     if bg_uart.any():
         time.sleep(.01)
         data = bg_uart.read()
@@ -192,17 +201,21 @@ while True:
                     continue
                 print(f"{time.ticks_ms()}: <- {line.strip('\r\n')}")
 
+                # waiting for APP RDY, the wake up call for BG77 module
                 if "APP RDY" in line:
                     print("     WAKEUP!!    ")
+
+                    # reconnecting to the network
                     if not module.__setCEREG(4):
-                        raise OSError("BG77 Error setting CEREG")
-                    ret = module.sendCommand(f"AT+CEREG?\r\n", "OK\r\n", 10)
+                        print("BG77 Error setting CEREG")
+                    ret = module.sendCommand(f"AT+CEREG?\r\n", "OK\r\n", 5)
 
                     module.sendCommand("AT+QCSCON=1\r\n")
                     module.sendCommand("AT+QMTCFG=\"version\",1,4\r\n")
                     module.sendCommand("AT+QMTOPEN=1,\"147.229.146.40\",1883\r\n", "+QMTOPEN: 1,0", 2)
                     module.sendCommand("AT+QMTCONN=1,\"27a26100-fd78-11ee-b0f6-299dee8b9dbf\",\"gCVn0rC7cOM7rxvYfo5l\",\"\"\r\n", "+QMTCONN: 1,0,0", 2)
 
+                    # send data if the modulue is connected
                     if "+CEREG: 4" in ret:
                         nwinfo = module.getNWInfo()
                         if nwinfo is not None:
@@ -215,6 +228,7 @@ while True:
                                 "band": nwinfo.Band,
                                 "cellid": nwinfo.CellID,
                                 "tac": nwinfo.TAC,
+                                "rat": nwinfo.RAT,
                             }
                             time.sleep(1);
                             module.sendData_JSON(data, "telemetry")
